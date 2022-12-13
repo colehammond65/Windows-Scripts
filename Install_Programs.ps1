2b8c272994b771cd8be3fc6dd0a3470896b2932a
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2022-12-13 13:27:10 +1300

    Update Install_Programs.ps1

--- Install_Programs.ps1
+++ Install_Programs.ps1
@@ -5,21 +5,15 @@ $programsToInstall = Get-Content programs.txt
 $ErrorActionPreference = "Continue"
 
 # Update all installed packages
-winget update --all
+winget upgrade --all
 
-# Use the winget show command to check if each program is already installed
 foreach($program in $programsToInstall) {
   try {
-    $showOutput = winget show $program
-    if($showOutput -notmatch "not installed") {
-      # If the program is already installed, skip it
-      continue
-    }
-
     # Use the winget install command with the --force flag to install the program
     winget install $program --force
   } catch {
     # If an error occurs, log it so that we can investigate later
     Write-Error "Failed to install $program. Error message: $($_.Exception.Message)"
+    pause
   }
 }
