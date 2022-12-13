9bd895ee53d006579b10afdb04e7aa8b9d1b4da0
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2022-12-14 08:24:56 +1300

    Update Install_Programs.ps1

--- Install_Programs.ps1
+++ Install_Programs.ps1
@@ -10,7 +10,7 @@ winget upgrade --all
 foreach($program in $programsToInstall) {
   try {
     # Use the winget install command with the --force flag to install the program
-    winget install $program --force
+    winget install $program --force --quiet
   } catch {
     # If an error occurs, log it so that we can investigate later
     Write-Error "Failed to install $program. Error message: $($_.Exception.Message)"
