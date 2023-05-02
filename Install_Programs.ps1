541099d619abca5c1be6c6d4dc7d8fbdb2c391f5
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2023-05-02 20:20:50 +1200

    as

--- Install_Programs.ps1
+++ Install_Programs.ps1
@@ -1,16 +1,9 @@
-﻿# Define a list of programs to install
-$programsToInstall = Get-Content programs.txt
+﻿# Read the list of programs from the text file
+$programs = Get-Content -Path "programs.txt"
 
-# Update all installed packages
-winget upgrade --all
-
-foreach($program in $programsToInstall) {
-  try {
-    # Use the winget install command with the --force flag to install the program
+# Install each program using Winget
+foreach ($program in $programs) {
+    Write-Host "Installing $program ..."
     winget install $program
-  } catch {
-    # If an error occurs, log it so that we can investigate later
-    Write-Error "Failed to install $program. Error message: $($_.Exception.Message)"
-    pause
-  }
 }
+pause
