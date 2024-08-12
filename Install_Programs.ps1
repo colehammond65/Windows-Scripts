614627efa02623d06366ffb29f5cf78537157d9a
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2024-08-12 18:53:52 +1200

    Update

--- Install_Programs.ps1
+++
@@ -1,9 +0,0 @@
-﻿# Read the list of programs from the text file
-$programs = Get-Content -Path "programs.txt"
-
-# Install each program using Winget
-foreach ($program in $programs) {
-    Write-Host "Installing $program ..."
-    winget install $program
-}
-pause
